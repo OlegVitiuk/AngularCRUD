6ad0e38c7ca982a123c8ed5ee3bd3ffed02c0e08
Oleg <vitiukoleh@gmail.com>
2017-02-14 03:39:13 +0200

    location is working

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ myApp.factory('DataCache', function ($cacheFactory) {
 
 myApp.controller("myController",function($scope,$http,DataCache){
 
- 	// $scope.$emit('LOAD');
+ 	$scope.$emit('LOAD');
  	$scope.MyData = DataCache.get('data');
 
  	if(!$scope.MyData){
@@ -14,13 +14,15 @@ myApp.controller("myController",function($scope,$http,DataCache){
              then(function(response) {
              	$scope.MyData = response.data;
              	DataCache.put('data', response.data);
-             	// $scope.$emit('UNLOAD');
+             	$scope.$emit('UNLOAD');
             });
  	}
 
 	$scope.newUser={};
 	$scope.clickedUser={};
 	$scope.message = "";
+	$scope.distance = 0;
+	$scope.myLocation = { lat: 50.448140, lng: 30.452775 }
 
 	$scope.saveUser=function(){
 		$scope.users.push($scope.newUser);
@@ -58,12 +60,12 @@ myApp.controller("myController",function($scope,$http,DataCache){
 		return false;
 	};
 
-	// $scope.$on('LOAD',function(){
-	// 	$scope.loading = true;
-	// })
-	// $scope.$on('UNLOAD',function(){
-	// 	$scope.loading = false;
-	// })
+	$scope.$on('LOAD',function(){
+		$scope.loading = true;
+	})
+	$scope.$on('UNLOAD',function(){
+		$scope.loading = false;
+	})
 
 
 	$scope.sortColumn = "name";
@@ -78,10 +80,55 @@ myApp.controller("myController",function($scope,$http,DataCache){
 		if($scope.sortColumn==column){
 			return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
 		}
+		return "";
+	}
+
+	$scope.getLocation = function(){
+		return $scope.myLocation;
+	}
+
+	$scope.haversineDistance=function(a, b){
+
+		var atan2 = Math.atan2
+		var cos = Math.cos
+		var sin = Math.sin
+		var sqrt = Math.sqrt
+		var PI = Math.PI
+
+		 // (mean) radius of Earth (meters)
+		var R = 6378137
+
+		function squared (x) { return x * x }
+		function toRad (x) { return x * PI / 180.0 }
+
+
+		var aLat = a.latitude || a.lat
+		 var bLat = b.latitude || b.lat
+		 var aLng = a.longitude || a.lng
+		 var bLng = b.longitude || b.lng
+
+		 var dLat = toRad(bLat - aLat)
+		 var dLon = toRad(bLng - aLng)
+
+		  var f = squared(sin(dLat / 2.0)) + cos(toRad(aLat)) * cos(toRad(bLat)) * squared(sin(dLon / 2.0))
+		  var c = 2 * atan2(sqrt(f), sqrt(1 - f))
+
+		  return R * c //get distance in meters!
 	}
 
 $( document ).ready(function() {
      $scope.message="All users have been successfully downloaded!";
 });
 
+});
+
+myApp.directive("getDistance",function(){
+	return function(scope, element ,attributes){
+
+		var atrValue=attributes["getDistance"];
+		var data = scope[atrValue];
+		var distanceInKilometres = scope.haversineDistance(scope.getLocation(),data.address.geo)/1000;
+
+		scope.distance = parseFloat(distanceInKilometres.toFixed(2))
+	}
 });
